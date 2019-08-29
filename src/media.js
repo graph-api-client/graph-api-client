5278aa8353f22cb8b11d04d83353c54d6c856c93
t.menteshashvili <tamar.menteshashvili@techubdc.ge>
2019-08-29 09:32:29 +0400

    from: changing global variable  ------->   to: parameter

--- src/media.js
+++ src/media.js
@@ -67,7 +67,7 @@ const media = {
       const fieldsSelection = getFieldSelection(info.fieldNodes, '');
       const id = '17841418527734321';
       const token =
-        'EAAeZCOc98l00BAMFhBCUR0yQZBRd47drsWbejlOKqEQGzm81GuaWXKXomOPO8InGWPGTLN0iBT4t0ainxxblI3ztWmjeeDxddQyrHRQZC2uwgousoIfwqkg2nTHB7ZAxZCFZBR4DtZBb7QWz5apBkGS9Ns7xHAwgnlQm42A976KgfniniIy5ZCR8RalxlZB7VC8nVWk3bPRsZCaNMALngZCe7IgmZAxJ1BM160txQGVc2qvzRH79eWpCEi7x';
+        'EAAeZCOc98l00BAI3YJvCLY0oDH9DZBxIF4W4bAOIqjQIViqV1JZCoELK1gwmlv5SUzjYnXnR5cywPKZAeu8d9Mh4xCQmHMVKh6ZC7YZASWJHvZA8AClhNwW600doIvmQTZAtKTACedqNpTaSBubI3jSwBi6ZAZCHRpPQ5jQ2tg7ONciTCvhb1XjfOkgunuqZBvqHfydIjMkvpT2LpPwtSy4ug8yfVoiR5TEv45mBFKj1hcnQgZDZD';
       const baseURL = `https://graph.facebook.com/${id}?access_token=${token}&fields=${fieldsSelection}`;
 
       console.log('fieldsSelection', fieldsSelection);
--- src/utils.js
+++ src/utils.js
@@ -1,28 +1,32 @@
-let fieldsSelection = '';
-const getFieldSelection = fieldNodes => {
+const getFieldSelection = (fieldNodes, fieldsSelection) => {
+  let currentFieldSelection = fieldsSelection;
+
   for (let i = 0; i < fieldNodes.length; i++) {
     if (fieldNodes[i].selectionSet) {
-      fieldsSelection += `${fieldNodes[i].name.value}{`;
+      currentFieldSelection += `${fieldNodes[i].name.value}{`;
       let selections = fieldNodes[i].selectionSet.selections;
       const index = fieldNodes[i].selectionSet.selections.findIndex(
         f => f.name.value === 'data',
       );
       if (index > -1) {
-        console.log(fieldNodes[i].selectionSet.selections[index]);
+        // console.log(fieldNodes[i].selectionSet.selections[index]);
         selections =
           fieldNodes[i].selectionSet.selections[index].selectionSet.selections;
       }
-      getFieldSelection(selections);
-      fieldsSelection += '}';
+      currentFieldSelection = getFieldSelection(
+        selections,
+        currentFieldSelection,
+      );
+      currentFieldSelection += '}';
     } else {
-      fieldsSelection += `${fieldNodes[i].name.value}`;
+      currentFieldSelection += `${fieldNodes[i].name.value}`;
     }
 
     if (fieldNodes.length - 1 !== i) {
-      fieldsSelection += `,`;
+      currentFieldSelection += `,`;
     }
   }
-  return fieldsSelection;
+  return currentFieldSelection;
 };
 
 module.exports = getFieldSelection;
